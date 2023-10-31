6c6980e75eea42336365a7ce9efd665b4e094944
ViniHGV <ixxvinicius@gmail.com>
2023-10-31 10:33:02 -0300

    Fazendo Lógica para exibição de mais pratos

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,8 +2,22 @@
 import React from "react";
 import { CardMenu } from "./CardMenu";
 import { MenuData } from "../../data/data";
+import { useState, useEffect } from "react";
+import { ICardMenu } from "../../types/types";
 
 export const Menu = () => {
+  const [MenuVisible, setMenuVisible] = useState<ICardMenu[]>([]);
+  const [quantityData, setQuantityData] = useState(8);
+
+  useEffect(() => {
+    let MenuSlicer: ICardMenu[] = MenuData.slice(0, quantityData);
+    setMenuVisible(MenuSlicer);
+  }, [quantityData]);
+
+  const handleQuantityData = () => {
+    setQuantityData(quantityData + 2);
+  };
+
   return (
     <div className="py-24 px-5 flex flex-col items-center">
       <div className="text-center flex flex-col gap-5">
@@ -15,8 +29,8 @@ export const Menu = () => {
           Uma Viagem de Sabores para Satisfazer Todos os Paladares
         </p>
       </div>
-      <div className="mt-16 flex flex-col gap-8">
-        {MenuData.map((data, index) => (
+      <div className="mt-16 flex flex-col gap-8 mb-8">
+        {MenuVisible.map((data, index) => (
           <CardMenu
             key={index}
             image={data.image}
@@ -26,6 +40,14 @@ export const Menu = () => {
           />
         ))}
       </div>
+      {quantityData === MenuVisible.length && (
+        <button
+          className="w-auto px-8 py-3 bg-redDefault rounded-full text-white"
+          onClick={handleQuantityData}
+        >
+          Ver Mais{" "}
+        </button>
+      )}
     </div>
   );
 };
