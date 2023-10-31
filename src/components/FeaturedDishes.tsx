62f5adfd22fdd2e4604422aaeaa3cb6e80ff118e
ViniHGV <ixxvinicius@gmail.com>
2023-10-31 13:38:33 -0300

    Adicionando Carousel de Pratos em destaques

--- src/components/FeaturedDishes.tsx
+++ src/components/FeaturedDishes.tsx
@@ -1,24 +1,28 @@
 import Image from "next/image";
 import React from "react";
 import ExampleSwiper from "../../public/ExampleSwiper.png";
+import SwiperFeatured from "./SwiperFeatured/SwiperFeatured";
 
 export const FeaturedDishes = () => {
   return (
-    <div className="bg-redDefault text-white py-24 px-5">
-      <div>
-        <div className="flex flex-col gap-3">
-          <p className="font-black text-center">Nossos Pratos em Destaque</p>
-          <h2 className="font-black text-5xl text-center">
-            Sabor, Criatividade e Experiências Únicas
-          </h2>
+    <div className="bg-redDefault">
+      <div className="bg-redDefault text-white pt-24 px-5">
+        <div>
+          <div className="flex flex-col gap-3">
+            <p className="font-black text-center">Nossos Pratos em Destaque</p>
+            <h2 className="font-black text-5xl text-center">
+              Sabor, Criatividade e Experiências Únicas
+            </h2>
+          </div>
+          <p className="text-lg font-semibold text-center mt-5">
+            Explore nossa seleção de pratos em destaque, cuidadosamente
+            preparados para surpreender seu paladar.
+          </p>
         </div>
-        <p className="text-lg font-semibold text-center mt-5">
-          Explore nossa seleção de pratos em destaque, cuidadosamente preparados
-          para surpreender seu paladar.
-        </p>
       </div>
-      <div className="flex items-center justify-center mt-9">
-        <Image alt="" src={ExampleSwiper} />
+      <div className="flex items-center pb-24 justify-center mt-9">
+        <SwiperFeatured />
+        {/* <Image alt="" src={ExampleSwiper} /> */}
       </div>
     </div>
   );
---
+++ src/components/SwiperFeatured/SwiperFeatured.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import ImageSwiper from "../../../public/ImageSwiper.png";
+
+// Import Swiper styles
+import "swiper/css";
+// import "swiper/css/navigation";
+
+import "./styles.css";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+import Image from "next/image";
+import { CardMenu } from "../CardMenu";
+import { MenuData } from "../../../data/data";
+
+export default function SwiperFeatured() {
+  return (
+    <>
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        className="mySwiperFeatured"
+      >
+        {MenuData.map((data, index) => (
+          <SwiperSlide key={index}>
+            <CardMenu
+              content={data.content}
+              image={data.image}
+              price={data.price}
+              title={data.title}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
