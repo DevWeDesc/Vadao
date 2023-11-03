40eb1f43ac83391bd891822164b9dfbd5dfbfd75
ViniHGV <ixxvinicius@gmail.com>
2023-11-03 09:44:06 -0300

    Editando Quantidade de pratos por linha no menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -19,7 +19,7 @@ export const Menu = () => {
   };
 
   return (
-    <div className="py-24 px-5 flex flex-col items-center lg:px-16">
+    <div className="py-24 px-5 flex flex-col items-center lg:px-16 2xl:px-32">
       <div className="text-center flex flex-col gap-5">
         <div className="flex flex-col gap-2">
           <p className="text-base font-black">Nosso Menu</p>
@@ -29,7 +29,7 @@ export const Menu = () => {
           Uma Viagem de Sabores para Satisfazer Todos os Paladares
         </p>
       </div>
-      <div className="mt-16 grid grid-cols-2 gap-4 mb-8 lg:grid-cols-4 lg:gap-5">
+      <div className="mt-16 grid grid-cols-2 gap-4 mb-8 lg:grid-cols-4 lg:gap-5 2xl:grid-cols-5">
         {MenuVisible.map((data, index) => (
           <CardMenu
             isVertical={true}
