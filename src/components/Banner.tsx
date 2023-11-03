c27dd84b0b1f928094947416a832615b853bed7f
ViniHGV <ixxvinicius@gmail.com>
2023-11-03 09:26:22 -0300

    Corrigindo anner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,15 +7,15 @@ export const Banner = () => {
     <div className="bg-redDefault flex flex-col gap-12 py-24 px-5W lg:w-full lg:flex-row-reverse">
       <Image
         alt=""
-        className="lg:absolute lg:right-[18%] lg:w-96 lg:h-96"
+        className="lg:absolute lg:right-[18%] lg:w-[50vh] lg:h-[50vh]"
         src={ImageBanner}
       />
       <div>
-        <h2 className="text-[64px] text-white leading-tight opacity-40 font-extraBlack lg:text-9xl">
+        <h2 className="text-[64px] text-white leading-tight opacity-40 font-extraBlack lg:text-9xl 2xl:text-[12rem] 2xl:pl-16">
           Sabor Inigualável
         </h2>
-        <div className="lg:px-24">
-          <h3 className="text-[1.68rem] text-white font-extrabold lg:mt-16 lg:text-5xl">
+        <div className="lg:px-24 2xl:px-96">
+          <h3 className="text-[1.68rem] text-white font-extrabold lg:mt-16 lg:text-5xl 2xl:text-6xl">
             Uma Experiência Culinária que Encanta os Sentidos
           </h3>
         </div>
