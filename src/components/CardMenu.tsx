3c492273edbdf1c13ae33a4f95129b44da7932d9
ViniHGV <ixxvinicius@gmail.com>
2023-11-06 10:21:39 -0300

    Editando Card

--- src/components/CardMenu.tsx
+++ src/components/CardMenu.tsx
@@ -30,14 +30,14 @@ export const CardMenu = ({
     >
       <div
         className={`flex items-center w-full justify-center lg:max-h-[185px] bg-black/5 opacity-${opacityImage} transition-all bg-fixed rounded-t-lg ${
-          colorShadow === "#E6D6C8" && "max-h-[11.25rem] sm:max-h-[13.8rem]"
+          colorShadow === "#E6D6C8" && "max-h-[104.58px] sm:max-h-[13.8rem]"
         }`}
         // max-h-[6.5rem]
       >
         <Image
           alt=""
           className={`w-full object-contain lg:max-h-[195px] rounded-t-lg ${
-            colorShadow === "#E6D6C8" && "max-h-[11.25rem] sm:max-h-[13.8rem]"
+            colorShadow === "#E6D6C8" && "max-h-[104.58px] sm:max-h-[13.8rem]"
           }`}
           // max-h-[6.5rem]
           src={image}
