8310df06e4ab270fe9789043779ecd7337fb4c44
ViniHGV <ixxvinicius@gmail.com>
2023-11-07 13:19:36 -0300

    Editando Description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,8 @@ const roboto = Roboto_Flex({
 
 export const metadata: Metadata = {
   title: "Vadão Restaurante",
-  description: "Vadão Restaurante",
+  description:
+    "Torne sua refeição uma experiência inesquecível no Restaurante do Vadão, onde a tradição culinária brasileira ganha vida. Descubra uma variedade de pratos autênticos que celebram os sabores ricos e vibrantes do Brasil. De suculentas carnes grelhadas a pratos típicos como feijoada e acarajé, nossa cozinha é uma viagem gastronômica que agrada a todos os paladares. Sinta-se em casa com nossa hospitalidade calorosa e desfrute de uma refeição que é uma verdadeira celebração da cultura brasileira. Faça uma reserva agora e mergulhe no mundo de sabores que só o Brasil pode oferecer.",
   verification: {
     google: "hIF2FiUO5JW0zMk70felqVUGDRVWeGcSjp_7fnzrmxg",
   },
